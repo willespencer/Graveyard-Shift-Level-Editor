3566463560b13b2cae4c19a078caed0fb6638204
Will Spencer <will.e.spencer@gmail.com>
2021-03-24 13:38:03 -0400

    Create a map from inputted size

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="dashboard">
+    <div class="sizeWrapper">
+      <span>What size should the level be (in tiles, width x height)?</span>
+      <div class="sizeInputs">
+        <input class="input" v-model="cols" /> x
+        <input class="input" v-model="rows" />
+      </div>
+      <button class="button" @click="updateDimensions">Create Map</button>
+    </div>
+    <level-map :height="height" :width="width" />
+  </div>
+</template>
+
+<script>
+import LevelMap from "@/components/LevelMap.vue";
+
+export default {
+  components: { LevelMap },
+  data() {
+    return {
+      width: 0,
+      height: 0,
+      rows: "",
+      cols: "",
+    };
+  },
+  methods: {
+    updateDimensions() {
+      this.width = Number(this.cols);
+      this.height = Number(this.rows);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sizeWrapper {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.sizeInputs {
+  margin-top: 0.5rem;
+  display: flex;
+}
+
+.input {
+  max-width: 2rem;
+  margin: 0 0.5rem;
+}
+
+.button {
+  margin-top: 0.5rem;
+}
+</style>
---
+++ src/components/LevelMap.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="levelmap">
+    <div class="row" v-for="(row, r) in height" v-bind:key="r">
+      <div class="col" v-for="(col, c) in width" v-bind:key="c"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    height: Number,
+    width: Number,
+  },
+};
+</script>
+
+<style scoped>
+.levelmap {
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.row {
+  display: flex;
+}
+
+.col {
+  background-color: red;
+  border: 1px solid black;
+  height: 50px;
+  width: 50px;
+}
+</style>
